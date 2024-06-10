b9f33c12ccd0b924f424bc5271aac65df339664d
rooch Vertus <jackethanvertus@gmail.com>
2024-06-10 20:21:11 -0300

    Novo Class Cadastro Pessoa

--- src/CadastroPessoa.java
+++ src/CadastroPessoa.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class CadastroPessoa {
-}
+public class CadastroPessoa {
+        private int id;
+        private String nome;
+        private int idade;
+
+        public CadastroPessoa(int id, String nome, int idade) {
+            this.id = id;
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        @Override
+        public String toString() {
+            return "Pessoa{" +
+                    "id=" + id +
+                    ", nome='" + nome + '\'' +
+                    ", idade=" + idade +
+                    '}';
+        }
+    }
+
+
